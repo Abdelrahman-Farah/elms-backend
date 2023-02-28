b7b4803a7c18aecd75473714ed262a5017d445c2
Abdelrahman Farah Mohammed <abdelrahman.mohammed@bld.ai>
2023-02-28 12:45:01 +0200

    User Authentication (Login and signup)

---
+++ core/serializers.py
@@ -0,0 +1,10 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password']
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name', 'email', 'username']
---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def test(request):
+    return Response('it is working')
