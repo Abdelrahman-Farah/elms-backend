56d1a574f2b6e58f48e6d2a86513290607a6bb3d
Abdelrahman-Farah <73790740+Abdelrahman-Farah@users.noreply.github.com>
2023-03-07 17:20:59 +0200

    Delete seeds directory

--- seeds/django_content_type.sql
+++
@@ -1,7 +0,0 @@
-insert into elms.django_content_type (id, app_label, model)
-values  (1, 'admin', 'logentry'),
-        (3, 'auth', 'group'),
-        (2, 'auth', 'permission'),
-        (4, 'contenttypes', 'contenttype'),
-        (6, 'core', 'user'),
-        (5, 'sessions', 'session');
--- seeds/django_session.sql
+++
@@ -1,2 +0,0 @@
-insert into elms.django_session (session_key, session_data, expire_date)
-values  ('haafnps6lq0vbxi1is161qb4kjhy2kwi', '.eJxVjLsOwjAMAP8lM4rq1nnAyM43RI7jkAJKpaadEP-OInWA9e50bxVo30rYm6xhTuqiQJ1-WSR-Su0iPajeF81L3dY56p7owzZ9W5K8rkf7NyjUSt9aI5Zw9CmPyTgr0YDDaPOEQpMDRAaxPERGHx0hIzCgS24wZ6Ds1ecL2cQ3nA:1pZXxB:yT20EChTMd8YLvTPYRvsi3Bji2C4T6_VMAHyIyUccnQ', '2023-03-21 14:06:09.045761');
