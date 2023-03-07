4e4be0359d278658e8f03b848bbd4a536a6b81f3
Abdelrahman Farah Mohammed <abdelrahman.mohammed@bld.ai>
2023-03-07 17:14:56 +0200

    seeds

---
+++ seeds/django_content_type.sql
@@ -0,0 +1,7 @@
+insert into elms.django_content_type (id, app_label, model)
+values  (1, 'admin', 'logentry'),
+        (3, 'auth', 'group'),
+        (2, 'auth', 'permission'),
+        (4, 'contenttypes', 'contenttype'),
+        (6, 'core', 'user'),
+        (5, 'sessions', 'session');
