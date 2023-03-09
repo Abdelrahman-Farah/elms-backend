cb8789077a54643584156918bc89114347ac6b8d
Abdelrahman Farah Mohammed <abdelrahman.mohammed@bld.ai>
2023-03-09 19:32:01 +0200

    erase testing view

--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('test2/', views.test),
-]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,3 @@
-from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.shortcuts import render
 
-@api_view()
-def test(request):
-    return Response('it is working')
+# Create your views here.
