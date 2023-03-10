294988b5c404c0136229c6be7a1263666af28150
Mazen Barakat Alyemeni <mazen.alyemeni@bld.ai>
2023-03-10 19:59:34 +0200

    User Authentication (Login and signup)

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+#model
 class User(AbstractUser):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
